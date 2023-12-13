66d93a576ba9e2a041d4fda05b769c66abe284ff
Mane <manefelice@gmail.com>
2023-12-13 17:11:19 -0300

    Nueva pagina Animal

---
+++ controlador/animal_controlador.php
@@ -0,0 +1,23 @@
+<?php
+require_once("modelo/animal.php");
+class AnimalControlador extends Animal{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function indexAnimal(){
+        require_once("vista/animales.php");
+    }
+
+    public function mostrarAnimal(){
+        if (isset($_REQUEST["id"])){
+            $animal= $this->consultarUno($_REQUEST["id"]);
+        }else{
+            $animal= $this;
+        }
+        require_once("vista/animal_formulario.php");        
+    }
+    public function guardarAnimal(){}
+    public function eliminarAnimal(){}
+}
+?>
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+// if(!isset($_REQUEST["controlador"])){
+//     require_once("controlador/usuario_controlador.php");
+//     $controlador = new Usuariocontrolador();
+//     $controlador->indexUsuario();
+// }else{
+//     $controlador = $_REQUEST["controlador"];
+//     $accion = $_REQUEST["accion"];
+//     require_once("controlador/$controlador"."_controlador.php");
+//     $controlador = ucwords($controlador)."Controlador";
+//     $controlador = new $controlador;
+//     call_user_func([$controlador,$accion]);
+// }
+
 if(!isset($_REQUEST["controlador"])){
     require_once("controlador/usuario_controlador.php");
     $controlador = new Usuariocontrolador();
