d54ae185ebaf312d49ff11f3ddb8b8a4344ca506
Mane <manefelice@gmail.com>
2023-12-13 19:59:24 -0300

    Puesta en funcionamiento de vista 'Animal'

--- controlador/animal_controlador.php
+++ controlador/animal_controlador.php
@@ -18,8 +18,18 @@ class AnimalControlador extends Animal{
         require_once("vista/animal_formulario.php");        
     }
     public function guardarAnimal(){
-        
+        $this->id = $_REQUEST["id"];
+        $this->nombre = $_REQUEST["nombre"];
+        $this->raza = $_REQUEST["raza"];
+        $this->sexo = $_REQUEST["sexo"];
+        $this->color = $_REQUEST["color"];
+        $this->edad = $_REQUEST["edad"];
+        $this->id>0?$this->actualizarAnimal():$this->insertarAnimal();
+        header("Location:index.php");
     }
-    public function eliminarAnimal(){}
+    public function eliminarAnimal(){
+        $this->delete($_REQUEST["id"]);
+        header("Location:index.php");
+    }
 }
 ?>
--- core/crud.php
+++ core/crud.php
@@ -1,4 +1,7 @@
 <?php
+// En van las operaciones a las bases de datos (crear,read, update, delete)
+// Ademas el constructor.
+// se agrega la conexion para la base de datos.
 require_once("core/conexion.php");
 class Crud extends Conexion{
 	private $pdo;
